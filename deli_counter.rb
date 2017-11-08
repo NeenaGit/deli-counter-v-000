c0275915b73f541c098a1537221035789e30d978
Neena van de Water <nkuriakose@hotmail.com>
2017-11-08 16:18:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,15 +4,13 @@ require 'pry'
 def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
-  elsif 
+  else
     intro_line = "The line is currently:"
         binding.pry
     katz_deli.each_with_index do | people, index |
-     intro_line << "#{index + 1}. #{people}" 
-    end
-  else
-    katz_deli
+     intro_line << "#{index + 1}. #{people}"
   end
+
 end
 
 def take_a_number(katz_deli, person_inline)
