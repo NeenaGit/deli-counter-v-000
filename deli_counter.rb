805294289b6d1ed22cb7e623c266dbef356da5ab
Neena van de Water <nkuriakose@hotmail.com>
2017-11-08 15:19:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+def line(katz_deli)
+  if katz_deli == []
+    puts "The line is currently empty."
+  elsif katz_deli != []
+    intro_line = "The line is currently:"
+    intro_line << katz_deli.each_with_index { | people, index |
+    "#{index + 1}. #{people}" }
+  else
+    puts katz_deli
+  end
+
+end
