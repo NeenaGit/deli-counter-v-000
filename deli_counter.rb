7727ab7fe827bcf6488b249ee248ad371ce93fb5
Neena van de Water <nkuriakose@hotmail.com>
2017-11-08 16:55:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,12 +18,11 @@ def take_a_number(katz_deli, name)
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."}
 end
 
-def now_serving(katz_deli, person)
+def now_serving(katz_deli)
   if katz_deli == []
     puts "There is nobody waiting to be served!"
   else
-    katz_deli.unshift(person).each{ | next_person |
-      puts "Currently serving #{next_person}."
-    }
+    katz_deli.shift
+      puts "Currently serving #{katz_deli[0]}."
   end
 end
